a06f84ad4b20f6b21bf4181e4ff3c306caeb1e6f
andre-ricardo-nave <Andre.ricardo@nave.org.br>
2026-06-05 16:29:38 -0300

    Classe

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,27 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+let personagem = {
+    nome: "Romeu Rômulo",
+    classe: "bardo",
+    nivel: 12,
+    equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+    pontosDeVida: 100,
+    pontosDeMagia: 100,
+    moedas: {
+        ouro: 20,
+        prata: 43,
+        bronze: 125,
+    } ,
+    pocoes: {
+        cura: 7,
+        magia: 5
+    },
+    nMusica: "Nome Musica",
+    LMusica: "Letra",
+    tocarMusica: function(nMusica, LMusica) {
+    console.log("Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso uma música para trazer ânimo a seus corpos e esperança para seus corações. Apresento-lhes a música " + [nMusica] + ". Em 1, 2, 3 ... " [LMusica])
+  }
+}
+
+export default personagem
