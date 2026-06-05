dee59153691fcd09819894fd7285c8f67bb053bd
kaique_Alex_Nave <kaique.alex@nave.org.br>
2026-06-05 16:15:39 -0300

    atividade 1,2,3 e 4 feitas, graça a Deus

--- src/AtvMetodoComplicado.js
+++ src/AtvMetodoComplicado.js
@@ -39,3 +39,62 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+    nome: "Romeu Rômulo",
+    classe: "bardo",
+    nivel: 12,
+    equipamento: [
+        "arco",
+        "violão",
+        "gaita",
+        "flechas de aço",
+        "botas de couro",
+        "capa de couro",
+        "chapéu de pena"
+    ],
+    pontosDeVida: 30,
+    pontosDeMagia: 100,
+    moedas: {
+        ouro: 20,
+        prata: 43,
+        bronze: 125
+    },
+    pocoes: {
+        cura: 0,
+        magia: 5
+    },
+
+    tocarMusica: function(musica, letra) {
+        console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+
+Em 1, 2, 3
+...
+${letra}`)
+    },
+
+comprarPocoes: function(quantidade) {
+    const custo = quantidade * 15 * 10; // convertido para bronze
+
+    let totalBronze =
+        (this.moedas.ouro * 100) +
+        (this.moedas.prata * 10) +
+        this.moedas.bronze;
+
+    if (totalBronze < custo) {
+        console.log("Você não tem dinheiro o suficiente, meu caro. Quer fazer um empréstimo?");
+        return;
+    }
+
+    totalBronze -= custo;
+
+    this.moedas.ouro = Math.floor(totalBronze / 100);
+    this.moedas.prata = Math.floor((totalBronze % 100) / 10);
+    this.moedas.bronze = totalBronze % 10;
+
+    this.pocoes.cura += quantidade;
+    }
+}
+
+export default personagem
