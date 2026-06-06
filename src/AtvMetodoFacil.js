5dd9479d6335f4b71c2610e1ceda939070736a9c
Filipedaspaçoca <adriano.santos@nave.org.br>
2026-06-06 13:06:01 -0300

    fet

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,26 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: "12",
+  equipamento: [
+    "arco",
+    "violão",
+    "gaita", 
+    "flechas de aço", 
+    "botas de couro", 
+    "capa de couro", 
+    "chapéu de pena",
+  ],
+tocarMusica: function(musica,letra) {
+   console.log("Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso " +
+    "uma música para trazer ânimo a seus corpos e esperança para seus corações. " +
+    "Apresento-lhes a música " + musica + ". " +
+    "Em 1, 2, 3 ... " +
+    letra
+  );
+}
+}
+export default personagem;
