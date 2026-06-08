bf0b533d6703490d0d7909e8de55ef8f90bb3e36
Luanna-Carla-nave <luanna.carla@nave.org.br>
2026-06-08 13:56:53 -0300

    feito.

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -19,4 +19,43 @@
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: [
+    "arco",
+    "violão",
+    "gaita",
+    "flechas de aço",
+    "botas de couro",
+    "capa de couro",
+    "chapéu de pena",
+  ],
+  pontosDeVida: 100,
+  pontosDeMagia: 100,
+  moedas: { ouro: 20, prata: 43, bronze: 125 },
+  pocoes: { cura: 7, magia: 5 },
 
+  tocarMusica(musica, letra) {
+    console.log(`
+Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+
+Em 1, 2, 3
+...
+${letra}
+`);
+  },
+};
+
+personagem.tocarMusica(
+  "Ode à Alegria",
+  `Alegria, bela centelha divina,
+Filha do Elísio!
+Ébrios de fogo, ó celestial,
+Adentramos teu santuário!`
+);
+
+export default personagem;
