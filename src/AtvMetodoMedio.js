531669f2edee6a884a3c48801ee797df985887c7
livia-barros-nave <livia.barros@nave.org.br>
2026-06-08 09:58:29 -0300

    atividade feita <3

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -16,3 +16,28 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: 'Romeu Rômulo',
+  classe: 'bardo',
+  itens: ['arco', 'violão', 'gaita', 'flechas de aço', 'botas de couro', 'capa de coruo', 'chapéu de pena'],
+  pontosDeVida: 30,
+  pontosDeMagia: 100,
+  moedas: {ouro: 20, prata: 43, bronze: 125},
+  pocoes: {cura: 7, magia: 5},
+  tocarMusica: function(nomeMusica, letraMusica) {
+    console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso uma música para trazer ânimo a seus corpos e esperança para seus corações.
+  Apresento-lhes a música ${nomeMusica}
+    
+  Em 1, 2, 3
+  ...
+  ${letraMusica}`
+    )
+  },
+  consumirPocao: function(){
+    this.pontosDeVida = 100;
+    this.pocoes.cura -= 1;
+  }
+}
+
+export default personagem
+
