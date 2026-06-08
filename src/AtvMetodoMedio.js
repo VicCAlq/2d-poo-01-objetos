4569fe45c332ee942295e89a9af073b58ab21c00
emilly-maciel-nave26 <emilly.maciel@nave.org.br>
2026-06-08 12:20:43 -0300

    emilly e kaique dupla

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -15,4 +15,45 @@
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
+const personagem = {
+    nome: "Romeu Rômulo",
+    classe: "bardo",
+    nivel: 12,
+    equipamento: [
+        "arco",
+        "violão",
+        "gaita",
+        "flechas de aço",
+        "botas de couro",
+        "capa de couro",
+        "chapéu de pena"
+    ],
+    pontosDeVida: 30,
+    pontosDeMagia: 100,
+    moedas: {
+        ouro: 20,
+        prata: 43,
+        bronze: 125
+    },
+    pocoes: {
+        cura: 7,
+        magia: 5
+    },
 
+    tocarMusica: function(musica, letra) {
+        console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+
+Em 1, 2, 3
+...
+${letra}`)
+    },
+
+    consumirPocao: function() {
+        this.pontosDeVida = 100
+        this.pocoes.cura--
+    }
+}
+
+export default personagem
