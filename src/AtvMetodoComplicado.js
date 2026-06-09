cd4add6fc0ddc784992010093c05116308f0cf0e
gabriel-cavalcanti-nave <gabriel.cavalcanti@nave.org.br>
2026-06-09 20:03:38 -0300

    atividade completa

--- src/AtvMetodoComplicado.js
+++ src/AtvMetodoComplicado.js
@@ -39,3 +39,75 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: [
+    "arco", 
+    "violão", 
+    "gaita", 
+    "flechas de aço", 
+    "botas de couro", 
+    "capa de couro", 
+    "chapéu de pena"
+  ],
+  pontosDeVida: 30,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 0,
+    magia: 5
+  },
+  
+  tocarMusica(musica, letra) {
+    console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+
+Em 1, 2, 3
+...
+${letra}`);
+  },
+
+  tomarPocaoCura() {
+    if (this.pocoes.cura > 0) {
+      this.pontosDeVida = 100;
+      this.pocoes.cura -= 1;
+      console.log("Poção de cura consumida!");
+    } else {
+      console.log("Você não tem poções de cura restantes!");
+    }
+  },
+
+  comprarPocaoCura(quantidade) {
+    const custoPorPocaoEmPrata = 15;
+    const custoTotalEmPrata = quantidade * custoPorPocaoEmPrata;
+
+    const totalPrataPersonagem = (this.moedas.ouro * 10) + this.moedas.prata + (this.moedas.bronze / 10);
+
+    if (totalPrataPersonagem < custoTotalEmPrata) {
+      console.log("Você não tem dinheiro o suficiente, meu caro. Quer fazer um empréstimo?");
+      return;
+    }
+
+    let saldoRestanteEmPrata = totalPrataPersonagem - custoTotalEmPrata;
+
+    this.moedas.ouro = Math.floor(saldoRestanteEmPrata / 10);
+    saldoRestanteEmPrata %= 10;
+
+    this.moedas.prata = Math.floor(saldoRestanteEmPrata);
+
+    this.moedas.bronze = Math.round((saldoRestanteEmPrata - this.moedas.prata) * 10);
+
+    this.pocoes.cura += quantidade;
+
+    console.log(`Compra realizada com sucesso! Você comprou ${quantidade} poções de cura.`);
+  }
+};
+
+export default personagem;
--- src/AtvObjeto.js
+++ src/AtvObjeto.js
@@ -20,3 +20,31 @@
   * as palavras "export default" seguido do nome do objeto, 
   * como "export default personagem"
   */
+
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: [
+    "arco", 
+    "violão", 
+    "gaita", 
+    "flechas de aço", 
+    "botas de couro", 
+    "capa de couro", 
+    "chapéu de pena"
+  ],
+  pontosDeVida: 100,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 7,
+    magia: 5
+  }
+};
+
+export default personagem;
