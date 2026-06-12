cdb4159d3dbf82ced26f66b3f38ee3c1022da95b
waldemir-viegas-nave <waldemir.viegas@nave.org.br>
2026-06-12 15:24:40 -0300

    Dois Completos

--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -1,18 +1,55 @@
 /*
-  * Exercício 3
+  * Exercício 2
   *
-  * Copie o personagem do exercício 2, e mude os "pontosDeVida" dele 
-  * de "100" para "30".
-  * 
-  * Após cantar a música no exercício 2, a platéia não gostou nada e moeu
-  * o personagem na pancada, reduzindo seus "pontosDeVida".
+  * Copie o personagem do exercício 1, e adicione nele um método chamado
+  * "tocarMusica", que recebe o nome de uma música e a letra desta música.
   *
-  * Crie agora um método onde o personagem pode consumir uma poção
-  * que ele possua para recuperar sua vida para o valor total.
+  * Ao ser chamado, o personagem deve exibir o texto abaixo, substituindo
+  * os valores entre colchetes pelos valores do nome da música e sua letra.
   *
-  * O método deve além de curar os pontos de vida, deve também remover uma 
-  * das poções de seu inventário.
+  * "
+  * Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+  * uma música para trazer ânimo a seus corpos e esperança para seus corações.
+  * Apresento-lhes a música [musica].
+  *
+  * Em 1, 2, 3
+  * ...
+  * [letra]
+  * "
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro",
+    "capa de couro", "chapéu de pena"
+  ],
+  pontosDeVida: 100,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 7,
+    magia: 5
+  },
+
+  tocarMusica: function (musica, letra) {
+
+    return`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+   uma música para trazer ânimo a seus corpos e esperança para seus corações.
+   Apresento-lhes a música ${musica}.
+  
+   Em 1, 2, 3
+   ...
+   ${letra}
+   `
+   }
+}
+
+export default personagem
