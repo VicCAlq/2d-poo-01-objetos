971c8f00dc0373418a9215a7a2437839b0e945f0
Gabriel-Leal-Nave <gabriel.leal@nave.org.br>
2026-06-16 18:47:26 -0300

    finalizado

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -1,5 +1,5 @@
 /*
-  * Exercício 2
+    * Exercício 2
   *
   * Copie o personagem do exercício 1, e adicione nele um método chamado
   * "tocarMusica", que recebe o nome de uma música e a letra desta música.
@@ -19,4 +19,32 @@
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
-
+ const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+  pontosDeVida: 100,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 7,
+    magia: 5
+  },
+  tocarMusica: function (musica, letra) {
+    console.log(`
+Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+ 
+Em 1, 2, 3
+...
+${letra}
+`);
+  }
+};
+ export default personagem;
--- src/AtvMetodoMedio.js
+++ src/AtvMetodoMedio.js
@@ -1,5 +1,5 @@
 /*
-  * Exercício 3
+   * Exercício 3
   *
   * Copie o personagem do exercício 2, e mude os "pontosDeVida" dele 
   * de "100" para "30".
@@ -15,4 +15,37 @@
   *
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
+ const personagem = {
+  nome: "Romeu Rômulo",
+  classe: "bardo",
+  nivel: 12,
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"],
+  pontosDeVida: 30,
+  pontosDeMagia: 100,
+  moedas: {
+    ouro: 20,
+    prata: 43,
+    bronze: 125
+  },
+  pocoes: {
+    cura: 7,
+    magia: 5
+  },
+  tocarMusica: function (musica, letra) {
+    console.log(`
+Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso
+uma música para trazer ânimo a seus corpos e esperança para seus corações.
+Apresento-lhes a música ${musica}.
+ 
+Em 1, 2, 3
+...
+${letra}
+`);
+  },
+  beberPocao: function(){
+    this.pontosDeVida = 100;
+    this.pocoes.cura = this.pocoes.cura - 1;
+  }
+};
+ export default personagem;
 
