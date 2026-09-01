91e9f19d4e89d34a3153897d0a274e0096e00721
samuel-assuncao-nave <samuel.assuncao@nave.org.br>
2026-09-01 14:37:42 -0300

    Add character object with music playing functionality

--- src/AtvMetodoFacil.js
+++ src/AtvMetodoFacil.js
@@ -20,3 +20,17 @@
   * Lembre-se de exportar o objeto do personagem no final do código.
   */
 
+let personagem = { nome: "Romeu Rômulo", 
+  classe: "bardo", 
+  nivel: 12, 
+  equipamento: ["arco", "violão", "gaita", "flechas de aço", "botas de couro", "capa de couro", "chapéu de pena"], 
+  pontosDeVida: 100, 
+  pontosDeMagia: 100, 
+  moedas: { ouro: 20, prata: 43, bronze: 125, } , 
+  pocoes: { cura: 7, magia: 5 }, 
+  tocarMusica: function(musica, letra) 
+  { return console.log(`Senhoras e senhores, gostaria de oferecer a vocês neste dia tempestuoso uma música para trazer ânimo a seus corpos e esperança para seus corações. Apresento-lhes a música ${musica}. Em 1, 2, 3 ... ${letra}`) } }
+let musicasLegais = { musica1: "teste" } 
+let letrasDeMusicasLegais = { letra1: "teste" } 
+personagem.tocarMusica(musicasLegais.musica1, letrasDeMusicasLegais.letra1) 
+export default personagem
